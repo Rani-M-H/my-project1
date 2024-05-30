ab1edbf21669ac24a6d588da9255347e90bd4815
Rani-M-H <108117164+Rani-M-H@users.noreply.github.com>
2024-05-30 15:57:23 +0530

    added feedback component

--- src/app/my-component/my-component.component.ts
+++ src/app/my-component/my-component.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { AddTaskComponent } from './add-task/add-task.component';
 import { EditTaskComponent } from './edit-task/edit-task.component';
+import { FeedbackComponent } from './feedback/feedback.component';
 
 @Component({
   selector: 'app-my-component',
   standalone: true,
-  imports: [AddTaskComponent , EditTaskComponent],
+  imports: [AddTaskComponent , EditTaskComponent , FeedbackComponent],
   templateUrl: './my-component.component.html',
   styleUrl: './my-component.component.css'
 })
