f0565acaf8ba099f104540ca95468f407fa0ee04
Rani-M-H <108117164+Rani-M-H@users.noreply.github.com>
2024-05-30 13:59:31 +0530

    added add-task,edit-task in my-component

--- src/app/my-component/my-component.component.ts
+++ src/app/my-component/my-component.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { EditTaskComponent } from './edit-task/edit-task.component';
 
 @Component({
   selector: 'app-my-component',
   standalone: true,
-  imports: [],
+  imports: [AddTaskComponent , EditTaskComponent],
   templateUrl: './my-component.component.html',
   styleUrl: './my-component.component.css'
 })
